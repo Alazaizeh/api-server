dbc3c26f6aa0b6d88079c5347cf2e65d2cf6c960
Omar Alazaizeh <omx302@gmail.com>
2021-08-18 19:26:38 +0300

    update

--- src/routes/clothes.js
+++ src/routes/clothes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const { Clothes } = require("../models/index");
 
 router.get("/clothes", getAllClothes);
+router.get("/clothes/:id", getOneClothes);
 router.post("/clothes", createClothes);
+router.delete("/clothes/:id", deleteClothes);
 
 async function getAllClothes(req, res) {
   let clothe = await Clothes.getAll();
@@ -16,6 +18,20 @@ async function createClothes(req, res) {
   let clothe = req.body;
   let clothes = await Clothes.create(clothe);
   res.status(201).json(clothes);
+}
+
+async function getOneClothes(req, res) {
+  const id = parseInt(req.params.id);
+
+  let clothe = await Clothes.get(id);
+  res.status(200).json(clothe);
+}
+
+async function deleteClothes(req, res) {
+  const id = parseInt(req.params.id);
+
+  let clothe = await Clothes.delete(id);
+  res.status(200).json(clothe);
 }
 
 module.exports = router;

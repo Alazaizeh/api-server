03122ffdc6d5eb60ba5dcfa7980e1106287e35aa
Omar Alazaizeh <omx302@gmail.com>
2021-08-22 08:11:38 +0300

    added more tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -23,4 +23,100 @@ describe("express server", () => {
     // assert
     expect(response.status).toBe(status);
   });
+  it("should check 500 errors", async () => {
+    // arrange
+    let param = "/bad";
+    let status = 500;
+    // act
+    const response = await request.get(param);
+    // assert
+    expect(response.status).toBe(status);
+  });
+  it("should response with object and 200 status on GET /food ", async () => {
+    // arrange
+    let param = "/food";
+    let status = 200;
+    // act
+    const response = await request.get(param);
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
+
+  it("should response with object and 200 status on GET /food/:id ", async () => {
+    // arrange
+    let param = "/food/1";
+    let status = 200;
+    // act
+    const response = await request.get(param);
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
+
+  it("should response with 200 status on DELETE /food/:id ", async () => {
+    // arrange
+    let param = "/food/1";
+    let status = 200;
+    // act
+    const response = await request.delete(param);
+    // assert
+    expect(response.status).toBe(status);
+  });
+
+  it("should response with object and 201 status on POST /food ", async () => {
+    // arrange
+    let param = "/food";
+    let status = 201;
+    // act
+    const response = await request.post(param).send({ name: "orange" });
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
+
+  it("should response with object and 200 status on GET /clothes ", async () => {
+    // arrange
+    let param = "/clothes";
+    let status = 200;
+    // act
+    const response = await request.get(param);
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
+
+  it("should response with object and 200 status on GET /clothes/:id ", async () => {
+    // arrange
+    let param = "/clothes/1";
+    let status = 200;
+    // act
+    const response = await request.get(param);
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
+
+  it("should response with 200 status on DELETE /clothes/:id ", async () => {
+    // arrange
+    let param = "/clothes/1";
+    let status = 200;
+    // act
+    const response = await request.delete(param);
+    // assert
+    expect(response.status).toBe(status);
+  });
+
+  it("should response with object and 201 status on POST /clothes ", async () => {
+    // arrange
+    let param = "/clothes";
+    let status = 201;
+    // act
+    const response = await request
+      .post(param)
+      .send({ name: "shirt", clothId: 2, price: 40 });
+    // assert
+    expect(response.status).toBe(status);
+    expect(typeof response.body).toEqual("object");
+  });
 });
